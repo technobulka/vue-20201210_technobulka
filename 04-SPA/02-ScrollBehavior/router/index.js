ecc45c9975f7493dbcb5593b8e19ea2d6fb39fc8
Stanislav Kondratiev <technobulka@gmail.com>
2021-01-02 14:52:40 +0300

    complete 04-SPA 02-ScrollBehavior

--- 04-SPA/02-ScrollBehavior/router/index.js
+++ 04-SPA/02-ScrollBehavior/router/index.js
@@ -3,8 +3,25 @@ import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
 
-export function scrollBehavior() {
-  // Место для решения
+export function scrollBehavior(to, from, savedPosition) {
+  if (to.hash) {
+    return {
+      selector: to.hash,
+    };
+  }
+
+  if (
+    from.matched.some((rec) => rec.meta.saveScrollPosition) &&
+    to.matched.some((rec) => rec.meta.saveScrollPosition)
+  ) {
+    return false;
+  }
+
+  if (savedPosition) {
+    return savedPosition;
+  } else {
+    return { x: 0, y: 0 };
+  }
 }
 
 export const router = new VueRouter({
