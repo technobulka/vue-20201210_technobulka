f8f89cd23655dd54402f7d244c6a3a87bae4a7a8
Stanislav Kondratiev <technobulka@gmail.com>
2021-01-03 19:47:20 +0300

    complete 04-SPA 06-Query

--- 04-SPA/06-Query/views/PageWithQuery.vue
+++ 04-SPA/06-Query/views/PageWithQuery.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="container">
-    <meetups-view />
+    <meetups-view
+      :view.sync="view"
+      :date.sync="date"
+      :participation.sync="participation"
+      :search.sync="search"
+    />
   </div>
 </template>
 
@@ -9,7 +14,56 @@ import MeetupsView from '../components/MeetupsView';
 
 export default {
   name: 'PageWithQuery',
+
   components: { MeetupsView },
+
+  data() {
+    return {
+      view: 'list',
+      date: 'all',
+      participation: 'all',
+      search: '',
+    };
+  },
+
+  watch: {
+    query(query) {
+      this.$router.push({ query }).catch(() => {});
+    },
+    $route: {
+      deep: true,
+      immediate: true,
+      handler(to) {
+        for (const key of Object.keys(to.query)) {
+          this[key] = to.query[key];
+        }
+      },
+    },
+  },
+
+  computed: {
+    query() {
+      let query = {};
+
+      if (this.view !== 'list') {
+        query.view = this.view;
+      }
+
+      if (this.date !== 'all') {
+        query.date = this.date;
+      }
+
+      if (this.participation !== 'all') {
+        query.participation = this.participation;
+      }
+
+      if (this.search !== '') {
+        query.search = this.search;
+      }
+
+      return query;
+    },
+  },
 };
 </script>
 
