158762ae382e43775f53b6b39a5124085fd5001e
Stanislav Kondratiev <technobulka@gmail.com>
2021-01-17 15:36:08 +0300

    WIP 06-deep-vue 03-DateInput

--- 06-deep-vue/03-DateInput/components/DateInput.vue
+++ 06-deep-vue/03-DateInput/components/DateInput.vue
@@ -1,6 +1,5 @@
 <template>
-  <AppInput>
-    <!-- Так можно передать все слоты в дочерний компонент -->
+  <AppInput :value="textValue" :type="type" v-bind="$attrs" v-on="listeners">
     <template v-for="slot of Object.keys($slots)" v-slot:[slot]>
       <slot :name="slot" />
     </template>
@@ -15,9 +14,86 @@ export default {
 
   components: { AppInput },
 
+  inheritAttrs: false,
+
   props: {
-    // Чтобы value не было в списке $attrs, его можно указать в списке параметров
-    value: {},
+    type: {
+      type: String,
+      default: 'date',
+      validator: (type) => ['date', 'time', 'datetime-local'].includes(type),
+    },
+    valueAsNumber: {
+      type: Number,
+    },
+    valueAsDate: {
+      type: Date,
+    },
+    value: {
+      type: String,
+    },
+  },
+
+  computed: {
+    textValue() {
+      if (this.valueAsNumber) {
+        return this.valueByType(this.valueAsNumber);
+      }
+
+      if (this.valueAsDate) {
+        return this.valueByType(this.valueAsDate);
+      }
+
+      return this.value;
+    },
+    listeners() {
+      return {
+        ...this.$listeners,
+        input: (value) => {
+          let date = this.dateFromValue(value);
+
+          this.$emit('update:valueAsNumber', date);
+          this.$emit('update:valueAsDate', new Date(date));
+        },
+        change: ($event) => {
+          let date = this.dateFromValue($event.target.value);
+
+          this.$emit('update:valueAsNumber', date);
+          this.$emit('update:valueAsDate', new Date(date));
+        },
+      };
+    },
+  },
+
+  methods: {
+    valueByType(value) {
+      value = new Date(value).toISOString();
+
+      let last = 16;
+      if (this.$attrs['step'] && this.$attrs['step'] < 60) {
+        last = 19;
+      }
+
+      switch (this.type) {
+        case 'date':
+          return value.slice(0, 10);
+        case 'time':
+          return value.slice(11, last);
+        case 'datetime-local':
+          return value.slice(0, last);
+      }
+    },
+    dateFromValue(value) {
+      let dateString = '1970-01-01T00:00:00.000Z'.split('');
+      let from = 0;
+
+      if (this.type === 'time') {
+        from = 11;
+      }
+
+      dateString.splice(from, value.length, ...value.split(''));
+
+      return Date.parse(dateString.join(''));
+    },
   },
 };
 </script>
